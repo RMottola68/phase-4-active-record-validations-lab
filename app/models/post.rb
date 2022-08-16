9610a82c52f30aea43049b2f55ecf0461dc81103
Ryan Mottola <rmottola68@gmail.com>
2022-08-15 20:30:19 -0400

    commit tree 4e52fce9eaff1e33fe8d663accdb05dcdd96d548

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :title, presence: true
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :title_is_clickbait
+
+
+    def title_is_clickbait
+        unless title != "True Facts"
+            errors.add(:title, "Title cannot be True Facts!")
+        end
+    end
+
 end
